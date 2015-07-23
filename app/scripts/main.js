e0d55e5f168afff3ed1ddd94cc47897e3cd00e92
zac1st1k <zacfirst@gmail.com>
2015-07-23 12:54:37 +1000

    + Piano Menu

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,22 @@
 // jshint devel:true
-console.log('\'Allo \'Allo!');
+
+$( document ).ready(function() {
+    $(".piano-key").click(function() {
+      $("#piano-menu").animate({
+        marginLeft: "-20%",
+        marginRight: "-50%", //invailid
+        width: "200%"
+      }, 1000).hide(1000);
+      $(this).animate({
+        paddingLeft: "100%"
+      }, 1000, function() {
+          $("body").css("background-color", "#2F4F55");
+        });
+      $("#homepage").animate({
+        marginLeft: "0"
+      }, 1000);
+      $('#side-menu').animate({
+        right: "10px"
+      }, 1000);
+    });
+});
